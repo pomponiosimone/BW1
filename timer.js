d59a6564f683e8a01349a4e680e2ab2ed47b6fe7
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-05-21 10:16:17 +0200

    adds timer script

--- timer.js
+++ timer.js
@@ -1,9 +1,30 @@
+//funzione per creare il timer e collegarlo al DOM
 const timer = (duration) => {
-  //imposto l'inzio del timer
-  let timerLeft = duration;
+  //creo la variabile che prenderà come valore la durata del timer(parametro)
+  let timeLeft = duration;
 
-  //richiamo l'elemento dove verrà mostrato il timer
-  const TimerElement = document.getElementById("timer");
+  //ottengo l'elemento del DOM dove verrà mostrato il timer
+  const timerElement = document.getElementById("Changesecond");
 
-  TimerElement.innerText = timerLeft;
+  //gli assegno come contenuto il timer
+  timerElement.textContent = timeLeft;
+
+  //creo funzione che aggiorna il timer
+  const updateTimer = () => {
+    //decremento il tempo rimanente
+    timeLeft--;
+
+    //aggiorno il valore nel DOM
+    timerElement.textContent = timeLeft;
+
+    //quando il tempo arriva a 0 si ferma il timer
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+    }
+  };
+
+  //altrimenti richiama la funzione la fuznione 'updateTimer' ogni secondo
+  const timerInterval = setInterval(updateTimer, 1000);
 };
+
+timer(60);
