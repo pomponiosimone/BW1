a348f615a7940d342ed0ffeac2be75ec5bb24c69
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-05-22 12:07:58 +0200

    aggiunto collegamento tra index e pagina2

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,8 +1,18 @@
-document
-  .getElemqueentById("proceedButton")
-  .addEventListener("click", function (event) {
+const checkAndRedirect = () => {
+  const button = document.getElementById("proceedButton");
+  button.addEventListener("click", function (event) {
+    //previene il comportamento default di refresh della pagina del bottone
+    event.preventDefault();
+    // Controlla se la checkbox è selezionata
     const checkbox = document.getElementById("condition");
     if (checkbox.checked) {
+      // Se la checkbox è selezionata, indirizza l'utente alla nuova pagina
       window.location.href = "pagina2.html";
+    } else {
+      // Altrimenti, mostra un messaggio di avviso
+      alert("Please check the checkbox before proceeding.");
     }
   });
+};
+//richiamo la funzione
+checkAndRedirect();
