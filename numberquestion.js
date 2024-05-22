46aafbbf080bfb21522de34e500c6b7177038d24
pomponiosimonegit config --global usern.email pomponio671@gmail.com <Pomponio671@gmail.com>
2024-05-22 12:50:44 +0200

    css e js bug fix

--- numberquestion.js
+++ numberquestion.js
@@ -1,8 +1,9 @@
 
 
 const changeNumberQ = function() { 
-    const numberQ = document.getElementById("numberQ");
-    numberQ.textContent = parseInt(numberQ.textContent) + 1;
+    const numberQ = document.getElementById("numberQ"); {
+    if (parseInt(numberQ.textContent) < 10 )
+        numberQ.textContent=parseInt(numberQ.textContent) + 1;}
 };
 document.addEventListener('click', function(event) {
    
